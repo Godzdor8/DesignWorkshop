d33e8cdda3c1b81078143c07e811b37abcd304f8
Godzdor8 <godzdor228@gmail.com>
2024-12-16 16:01:46 +0300

    fix

--- test_streamlit.py
+++ test_streamlit.py
@@ -1,11 +1,15 @@
 from main import get_result
 import cv2
+import numpy as np
+from PIL import Image
 
 
 def test_empty():
     assert True is True
 
+
 def test_picture():
     img = cv2.imread('pictures/image 1.jpg')
-    res = get_result(img)
+    img_pil = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    res = get_result(img_pil)
     assert res[0][0] > 0.9
